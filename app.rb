9e208e5658bf139db0c807b291cb0cccf278c03e
Sav Younts <savyounts@gmail.com>
2018-09-04 22:05:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,15 @@ class App < Sinatra::Base
     erb :new
   end
 
-  
+  post '/student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+
+    @courses = Course.all
+
+    erb :student 
+  end
 end
