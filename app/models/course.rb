05b817dbb6c5afc0bacc38f767301f0bce9a8f03
Sav Younts <savyounts@gmail.com>
2018-09-04 22:08:38 +0000

    Done.

---
+++ app/models/course.rb
@@ -0,0 +1,13 @@
+class Course
+  attr_reader :name, :topic
+
+  COURSES = []
+  def initialize(args)
+    @name = args[:name]
+    @topic = args[:topic]
+  end
+
+  def self.all
+    COURSES
+  end
+end
